f35253cec32fe3376db9ac22d5b0c99f9029951b
lmoran94 <lm579@exeter.ac.uk>
2021-06-10 16:02:23 +0100

    Fixed power change bug so total weight is added correctly.

--- raman_scripts/power_change.py
+++ raman_scripts/power_change.py
@@ -5,19 +5,23 @@ import csv
 import numpy as np
 from collections import OrderedDict
 
-
+total_weight=0.0
 with open("input/weightings.csv","r") as weights_file:
     csv_reader = csv.reader(weights_file, delimiter=',')
+    next(csv_reader)
     for weight in csv_reader:
         weight = float(weight[0])
+        total_weight += weight
 
 with open("input/mcrt_microcalcraman.json5","r") as jsonFile:
     data = json5.load(jsonFile)
 
 #(json5.parse(data))
 outerLight = data["light"]
-#innerLight = outerLight["power"]
-outerLight["power"] = float(np.sum(weight))
+#print("outerLight", outerLight)
+innerLight = outerLight["Here"]
+innerLight["power"] = float(total_weight)
+#print("outerlightpower", outerLight["power"])
 #print(innerLight)
 #innerLight = float(sys.argv[1])
 #print(innerLight)
